5cf2116dbfb49d1b6c772861400107aeb9338e1e
husong <305123882@qq.com>
2022-12-03 17:06:53 +0800

    feat:添加springboot整合H2数据库

---
+++ SpringBoot-h2/src/main/java/net/somta/springboot/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package net.somta.springboot.mapper;
+
+import net.somta.springboot.pojo.User;
+
+import java.util.List;
+
+
+public interface UserMapper {
+
+	int addUser(User user);
+	
+	int deleteUserById(Long id);
+	
+	int updateUserById(User user);
+
+	User queryUserById(Long id);
+	
+	List<User> queryUserList();
+}
---
+++ SpringBoot-h2/src/main/resources/db/schema.sql
@@ -0,0 +1,10 @@
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` bigint(20) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
