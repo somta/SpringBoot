26d78a5c2b5874b508d7886b709c4993ee447750
明天的地平线 <305123882@qq.com>
2023-12-28 23:47:45 +0800

    feat:添加java调用groovy的例子

--- SpringBoot-groovy/src/main/java/net/somta/springboot/groovy/controller/GroovyController.java
+++ SpringBoot-groovy/src/main/java/net/somta/springboot/groovy/controller/GroovyController.java
@@ -2,8 +2,10 @@ package net.somta.springboot.groovy.controller;
 
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
+import net.somta.springboot.groovy.param.ScriptParam;
 import net.somta.springboot.groovy.service.VariableManager;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -12,14 +14,27 @@ import java.util.Map;
 @RestController
 public class GroovyController {
 
-    @GetMapping("/groovy/execute")
-    public String executeScript(){
+    @PostMapping("/groovy/execute")
+    public String executeScript(@RequestBody ScriptParam scriptParam){
 
-        executeStringScript();
-        executeStringScriptByJavaVariable();
-        executeStringScriptByJavaMethod();
+        VariableManager variableManager = new VariableManager();
+        // 方案一：通过Binding 对象注入变量
+        //Binding binding = new Binding();
+        // 将 variableManager 对象设置到 Binding 对象中
+        //binding.setVariable("env", variableManager);
+        // 创建 GroovyShell，并传递 Binding 对象
+        //GroovyShell shell = new GroovyShell(binding);
+
+        // 方案二：通过setVariable注入变量
+        GroovyShell shell = new GroovyShell();
+        shell.setVariable("env", variableManager);
+
+        // 执行 Groovy 脚本
+        Object result = shell.evaluate(scriptParam.getScriptContent());
+        System.out.println(result);
         return "success";
     }
+
 
     public static void main(String[] args) {
         GroovyController controller = new GroovyController();
---
+++ SpringBoot-groovy/src/main/java/net/somta/springboot/groovy/param/ScriptParam.java
@@ -0,0 +1,13 @@
+package net.somta.springboot.groovy.param;
+
+public class ScriptParam {
+    private String scriptContent;
+
+    public String getScriptContent() {
+        return scriptContent;
+    }
+
+    public void setScriptContent(String scriptContent) {
+        this.scriptContent = scriptContent;
+    }
+}
